de3aeaed4cea49e7e8efbd9a95d84323627fcfc7
Froopps <45558150+Froopps@users.noreply.github.com>
2018-12-06 11:55:18 +0100

    checkbox_signup

--- frontend/signup.php
+++ frontend/signup.php
@@ -26,19 +26,20 @@
             <div class="content">
                 <form action="#signup_script" method="post">
                     <table id="signup-table">
-                        <tr><td>e-Mail*:</td><td><input type="text" name="mail" placeholder="e-mail" required></td></tr>
+                        <tr><td></td><td></td><td>Privato</td></tr>
+                        <tr><td>e-Mail*:</td><td><input type="text" name="mail" placeholder="e-mail" required></td><td><input type="checkbox" name="mail-check"></td></tr>
                         <tr><td>Password*:</td><td><input type="password" name="pwd" required></td></tr>
                         <tr><td>Conferma Password*:</td><td><input type="password" name="pwd-c" required></td></tr>
-                        <tr><td>Nome:</td><td><input type="text" name="nom" placeholder="Nome"></td></tr>
-                        <tr><td>Cognome:</td><td><input type="text" name="cog" placeholder="Cognome"></td></tr>
-                        <tr><td>Nickname:</td><td><input type="text" name="nick" placeholder="Nickname"></td></tr>
-                        <tr><td>Data di nascita:</td><td><input type="date" name="dataNa"></td></tr>
+                        <tr><td>Nome:</td><td><input type="text" name="nom" placeholder="Nome"></td><td><input type="checkbox" name="nom-check"></td></tr>
+                        <tr><td>Cognome:</td><td><input type="text" name="cog" placeholder="Cognome"></td><td><input type="checkbox" name="cog-check"></td></tr>
+                        <tr><td>Nickname:</td><td><input type="text" name="nick" placeholder="Nickname"></td><td><input type="checkbox" name="nick-check"></td></tr>
+                        <tr><td>Data di nascita:</td><td><input type="date" name="dataNa"></td><td><input type="checkbox" name="date-check"></td></tr>
                         <tr><td>Sesso:</td><td class="left"><select name="sex">
                             <option value="M">Male</option>
                             <option value="F">Female</option>
-                        </select></td></tr>
-                        <tr><td>Città:</td><td><input type="text" name="cit" placeholder="Città"></td></tr>
-                        <tr><td>Immagine del profilo:</td><td><input type="file" name="pic" accept="image/x-png,image/jpeg"></td></tr>
+                        </select></td><td><input type="checkbox" name="sex-check"></td></tr>
+                        <tr><td>Città:</td><td><input type="text" name="cit" placeholder="Città"></td><td><input type="checkbox" name="cit-check"></td></tr>
+                        <tr><td>Immagine del profilo:</td><td><input type="file" name="pic" accept="image/x-png,image/jpeg"></td><td><input type="checkbox" name="pic-check"></td></tr>
                         <tr><td colspan="2"><input type="submit"></td></tr>
                     </table>
                 </form>
