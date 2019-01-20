9dadded60f103126820998d2e084e089c2c69a8f
turatig <jack.turati@gmail.com>
2019-01-20 11:09:47 +0100

    WIP_search2

--- frontend/search_results.php
+++ frontend/search_results.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    include_once realpath($_SERVER["DOCUMENT_ROOT"]."/muy/common/setup.php");
 ?>
 
 <!DOCTYPE HTML>
@@ -24,7 +25,7 @@
         }
     ?>
 
-        <main>
+        <main class='search_page'>
             <div class="content">
                 <?php
                     if(isset($_GET["error"])){
@@ -34,9 +35,19 @@
                         echo "<span class='message_span'>".$_GET["msg"]."</span>";
                     }
                 ?>
-                <main>
-                    
-                </main>
+                    <?php
+                        $res=get_user_by_email($_SESSION['email'],$connected_db);
+                        $row=$res->fetch_assoc();
+                        $pro_pic=$_SERVER["DOCUMENT_ROOT"]."/../muy_res";
+                        $pro_pic="data:image/png;base64,".base64_encode(file_get_contents($pro_pic."/".stripslashes($row["foto"])));
+                        $pro_pic_alt="Spiacenti foto non trovata";
+                    ?>
+                    <ul class='search_results'>
+                        <li class='search_results_entry'>
+                        <?php
+                        echo "<img class='propic' src='$pro_pic' alt=$pro_pic_alt>";
+                        ?>
+                        </li>
             </div>
         </main>
         <script type="text/javascript" src="../common/script/search.js"></script>
